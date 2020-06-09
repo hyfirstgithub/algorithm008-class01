fb6ecf2c1c6ee8a9e88782e053f6181ab534ac88
haoy <YS.haoyang@h3c.com>
2020-06-09 12:28:59 +0800

    add

---
+++ Week_08/PPowerOfTwo.java
@@ -0,0 +1,39 @@
+//给定一个整数，编写一个函数来判断它是否是 2 的幂次方。 
+//
+// 示例 1: 
+//
+// 输入: 1
+//输出: true
+//解释: 20 = 1 
+//
+// 示例 2: 
+//
+// 输入: 16
+//输出: true
+//解释: 24 = 16 
+//
+// 示例 3: 
+//
+// 输入: 218
+//输出: false 
+// Related Topics 位运算 数学
+
+
+package com.leetCode.leetcode.editor.cn;
+//Java：2的幂, 231
+public class PPowerOfTwo{
+    public static void main(String[] args) {
+        Solution solution = new PPowerOfTwo().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        n = n -  (n & (-n));
+        return  n == 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
